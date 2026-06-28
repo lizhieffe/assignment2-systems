7b73ecd669fb89516b548dc092f8df6a5c594b1f
Zhi Li <lizhieffe@gmail.com>
2026-06-28 09:09:02 -0700

    Fix bug for assignment 2.7 that the replicated layers are still sharded

--- cs336_systems/assignment_7_fsdp.py
+++ cs336_systems/assignment_7_fsdp.py
@@ -63,6 +63,14 @@ def is_norm_layer(module: nn.Module) -> bool:
   return isinstance(module, norm_classes)
 
 
+def is_shard_layer(module: nn.Module) -> bool:
+  # return True
+  ret = isinstance(module, (model.Linear, model.Embedding))
+  if dist.get_rank() == 0:
+    print(f"{type(module)=} is_shard_layer={ret}")
+  return ret
+
+
 def get_named_layers(module: nn.Module) -> list[tuple[str, nn.Module]]:
   """Get all layers in sequence.
 
@@ -79,25 +87,10 @@ def get_named_layers(module: nn.Module) -> list[tuple[str, nn.Module]]:
 
 
 def get_shard_layers(all_layers: list[nn.Module]) -> list[nn.Module]:
-  return [layer for layer in all_layers if not is_norm_layer(layer)]
-
-
-def get_local_layers(
-  named_layers: list[tuple[str, nn.Module]], world_size: int, rank: int
-):
-  # layer to is_sharded
-  local_layers: Mapping[nn.Module, bool] = {}
-  layer_idx = 0
-
-  for _, layer in named_layers:
-    if is_norm_layer(layer):
-      local_layers[layer] = False
-    else:
-      if layer_idx % world_size == rank:
-        local_layers[layer] = True
-        layer_idx += 1
-
-  return local_layers
+  ret = [layer for layer in all_layers if is_shard_layer(layer)]
+  if dist.get_rank() == 0:
+    print(f"all layers = {len(all_layers)}, shard layers = {len(ret)}")
+  return ret
 
 
 class FSDP(torch.nn.Module):
@@ -106,9 +99,11 @@ class FSDP(torch.nn.Module):
   Weights: each GPU stores an equal slice of every weight tensor.
     In FWD, the weights are sync layer by layer; the sync overlays with computation.
     In the end of BWD, the full weight are sharded back.
+    The weight tensor for Norm layers are not sharded because the wight is higtweighted.
   Grads: each GPU stores an equal slice of grad corresponding to every weight tensor.
     In BWD, the grad are reduce-scatter layer by layer; the sync overlays with computation.
     In the beginning of next FWD, it is expected the caller will zero_grad().
+    The grad tensors for Norm layers are not sharded because their weights are not sharded.
   Optimizer state: the caller should maintain that. This FSDP impl works with either
     non-sharded optimizer or sharded optimizer. The optimizer state sharding is
     controlled by the optimizer.
@@ -128,11 +123,12 @@ class FSDP(torch.nn.Module):
     super().__init__()
 
     # TODO(lizhi): support compute_dtype.
+    self.module = module
+    self.compute_dtype = compute_dtype
 
     self.rank = dist.get_rank()
     self.world_size = dist.get_world_size()
 
-    self.module = module
     self.shard_params: set[nn.Parameter] = set()
 
     # Step 0 - get all layers that need to shard.
@@ -147,7 +143,6 @@ class FSDP(torch.nn.Module):
     for layer in self.all_shard_layers:
       for p in layer.parameters():
         chunks = list(p.detach().chunk(chunks=self.world_size, dim=0))
-        # print(f"{len(chunks)=}")
         local_chunk = torch.empty_like(chunks[0])
         if self.rank == 0:
           dist.scatter(local_chunk, scatter_list=chunks, src=0)
@@ -155,17 +150,22 @@ class FSDP(torch.nn.Module):
           dist.scatter(local_chunk, scatter_list=None, src=0)
         self.shard_params.add(p)
 
-        # Step 2 - delete params. Replace each param's full-size data with just
+        # Step 2 - Replace each param's full-size data with just
         # its local shard, so the rest of the original storage can be freed.
         p.data = local_chunk
 
-    # Step 3 - get all layers for forward layer sync.
+    # Here we don't sync for the replicated layers because they are
+    # RMSNorm and the initialized params are all 1s.
+    #
+    # TODO: verify this is the case for the customized code path.
+
+    # Step 3 - get all layers for forward layer sync.//
     for layer in self.all_shard_layers:
-      layer.register_forward_pre_hook(self.fwd_params_sync_hook)
+      layer.register_forward_pre_hook(self._fwd_params_sync_hook)
 
     for param in self.module.parameters():
       if param.requires_grad:
-        param.register_post_accumulate_grad_hook(self.bwd_grads_sync_hook)
+        param.register_post_accumulate_grad_hook(self._bwd_grads_sync_hook)
 
     # Step
     # local_layers = get_local_layers(
@@ -208,7 +208,7 @@ class FSDP(torch.nn.Module):
       # Replace the parameter data with the synced tensor.
       p.data = torch.cat(chunks, dim=0)
 
-  def fwd_params_sync_hook(self, module: nn.Module, input):
+  def _fwd_params_sync_hook(self, module: nn.Module, input):
     if module not in self.all_shard_layers_to_idx:
       raise ValueError("fwd_params_sync_hook is triggered on non shard layer.")
     layer_idx = self.all_shard_layers_to_idx[module]
@@ -219,14 +219,17 @@ class FSDP(torch.nn.Module):
       comm_layer = self.all_shard_layers[comm_idx]
       self._all_gather_params_for_layer(comm_layer)
 
-  def bwd_grads_sync_hook(self, param: nn.Parameter) -> None:
+  def _bwd_grads_sync_hook(self, param: nn.Parameter) -> None:
     assert param.requires_grad
-    chunks = list(param.grad.detach().chunk(chunks=self.world_size, dim=0))
-    local_chunk = torch.empty_like(chunks[0])
-    dist.reduce_scatter(
-      output=local_chunk, input_list=chunks, op=dist.ReduceOp.AVG
-    )
-    param.grad.data = local_chunk
+    if param in self.shard_params:
+      chunks = list(param.grad.detach().chunk(chunks=self.world_size, dim=0))
+      local_chunk = torch.empty_like(chunks[0])
+      dist.reduce_scatter(
+        output=local_chunk, input_list=chunks, op=dist.ReduceOp.AVG
+      )
+      param.grad.data = local_chunk
+    else:
+      dist.all_reduce(param.grad, op=dist.ReduceOp.AVG)
 
   def gather_full_params(self) -> dict[str, torch.Tensor]:
     """All-gather sharded params to reconstruct full parameter tensors.
